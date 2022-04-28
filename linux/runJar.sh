2f8c4740434d9e119bb721d3cb201a95ef8e536f
kim jae moon <moon4311@gmail.com>
2022-04-28 14:33:14 +0900

    Update runJar.sh
    add SLEEP

--- linux/runJar.sh
+++ linux/runJar.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
-TYPE=mall
+TYPE=colander
 
 CONFIG_INFO="--spring.config.location=file:../conf/application.yml"
 TIME_ZONE="Asia/Seoul"
 PROJECT="${TYPE}*.war"
-JAVA_OPTS=" ${CONFIG_INFO} -XX:MaxMetaspaceSize=512m -XX:+UseG1GC -Xss2048k -Xms512m -Xmx512m -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
+JAVA_OPTS=" ${CONFIG_INFO} -XX:MaxMetaspaceSize=512m -XX:+UseG1GC -Xss4096k -Xms512m -Xmx512m -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
 JAVA_LIB=""
 
-runPid=$(pgrep -f bin/${TYPE})
+
+runPid=$(pgrep -f $TYPE)
+runPortCount=$(ps -ef | grep $TYPE | grep -v grep | wc -l)
 if [ -z $runPid ]; then
-    echo "No servers are running"
+    echo "[$TYPE] server not running"
+else
+  SLEEP=5
+  echo "[$TYPE] kill pid : $runPid "
+  kill -TERM $runPid
+  while [ $runPortCount -gt 0 ]; do
+    runPortCount=$(ps -ef | grep $TYPE | grep -v grep | wc -l)
+    if [ $SLEEP -ge 0 ]; then
+      sleep 1
+    else
+      break;
+    fi
+    SLEEP=`expr $SLEEP - 1 `
+  done
 fi
 
-runPortCount=$(ps -ef | grep bin/${TYPE} | grep -v grep | wc -l)
 if [ $runPortCount -gt 0 ]; then
-   kill -TERM $runPid
-   #PORT=5100
-   echo "kill $runPid"
+  echo "[$TYPE] $runPid kill fail"
+else
+  echo "[$TYPE] Starting..."
+  nohup java -jar -Du=$TYPE -Duser.timezone=${TIME_ZONE} ../bin/${PROJECT} ${JAVA_OPTS} < /dev/null > ../log/std.out 2> ../log/std.err &
 fi
-echo "Server $TYPE Starting..."
-
-
-nohup java -jar -Duser.timezone=${TIME_ZONE} ../bin/${PROJECT} ${JAVA_OPTS} < /dev/null > ../log/std.out 2> ../log/std.err &
