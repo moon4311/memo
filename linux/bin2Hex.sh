b622edc563d1e2e9d2a4a76d4561c8a12f016c3f
kim jae moon <moon4311@gmail.com>
2022-05-24 10:25:50 +0900

    Update bin2Hex.sh

--- linux/bin2Hex.sh
+++ linux/bin2Hex.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 FILE_PATH='/home/timopass/middleWare/firmware/'
 FILE_SIZE=$(ls -al ${FILE_PATH} |grep .bin |awk '{print $5 }')
+#FILE_SIZE=$(ls -al /home/timopass/middleWare/firmware/ |grep .bin |awk '{print $5 }')
 
-# version Linux 7 ~
+# if $8 isn't fileName ,  choice A or B
+# A.  add .bash_profile  ->  alias ls='ls --color=auto --time-style=long-iso'
+# B.  $8 -> $9
 FILE_NAME=$(ls -al $FILE_PATH |grep .bin |awk '{print $9 }')
 if [ $FILE_NAME = '']; then
-# version Linux 6
   FILE_NAME=$(ls -al $FILE_PATH |grep .bin |awk '{print $8 }')
 fi
-
+echo "[$FILE_NAME]"
+#FILE_NAME=$(ls -al /home/timopass/middleWare/firmware/ |grep .bin |awk '{print $9 }')
 FILE_NEW_NAME=$(echo "$FILE_NAME" | sed "s/bin/hex/g")
+#echo $FILE_NAME " -> " $FILE_NEW_NAME
+echo $FILE_PATH
+echo $FILE_NAME
+echo $FILE_NEW_NAME
 
 xxd -l $FILE_SIZE -p -c $FILE_SIZE ${FILE_PATH}${FILE_NAME} > ${FILE_PATH}${FILE_NEW_NAME}
-
-# echo $FILE_NAME "move ./backup/"
 mv ${FILE_PATH}${FILE_NAME} ${FILE_PATH}backup/
+echo $FILE_NAME "move ./backup/"
